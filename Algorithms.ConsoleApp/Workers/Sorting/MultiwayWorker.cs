a9318f74b2a476e01f15a6a96a69fca54e93b77e
vladdnc <vladdnc@mail.ru>
2015-08-24 15:43:06 +0300

    Added Multiway class

---
+++ Algorithms.ConsoleApp/Workers/Sorting/MultiwayWorker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Algorithms.ConsoleApp.Attributes;
+using Algorithms.ConsoleApp.Interfaces;
+using Algorithms.Core.InOut;
+using Algorithms.Core.Sorting;
+
+namespace Algorithms.ConsoleApp.Workers.Sorting
+{
+    [ConsoleCommand("Multiway", "Provides a client for reading in several sorted text files and merging them together into a single sorted text stream.")]
+    public class MultiwayWorker : IWorker
+    {
+        public void Run()
+        {
+
+            var filesList = new List<string> { "m1.txt", "m2.txt", "m3.txt" };
+            var wordsList = new List<List<string>>();
+
+            foreach (var fileName in filesList)
+            {
+                var @in = new In($"Files\\Sorting\\{fileName}");
+                var words = @in.ReadAllStrings();
+                wordsList.Add(words.ToList());
+            }
+
+            var multiway = new Multiway(wordsList);
+
+            multiway.ShowWords();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Algorithms.Core/Sorting/Multiway.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.Core.Sorting
+{
+    /// <summary>
+    /// The <tt>Multiway</tt> class provides a client for reading in several
+    /// sorted text files and merging them together into a single sorted
+    /// text stream.
+    /// This implementation uses a {@link IndexMinPQ} to perform the multiway
+    /// merge.
+    /// </summary>
+    public class Multiway
+    {
+        private readonly IndexMinPQ<string> _pq;
+        private readonly int _n;
+        public Multiway(List<List<string>> lists)
+        {
+            var n = lists.Sum(l => l.Count);
+            _n = n;
+            _pq = new IndexMinPQ<string>(n);
+
+            foreach (var list in lists)
+            {
+               InsertWords(list);
+
+            }
+            
+        }
+
+        private void InsertWords(IEnumerable<string> list)
+        {
+            foreach (var word in list)
+            {
+                var indexEmpty = 0;
+
+                for (var i = 0; i < _n; i++)
+                {
+                    if (!_pq.Contains(i))
+                    {
+                        indexEmpty = i;
+                        break;
+                    }
+                }
+
+                _pq.Insert(indexEmpty, word);
+                if (_pq.Size() == _n)
+                {
+                    _pq.DelMin();
+                }
+                
+            }
+        }
+
+        public void ShowWords()
+        {
+            // print results 
+            foreach (var item in _pq)
+            {
+                Console.WriteLine(_pq.KeyOf(item));
+            }
+        }
+    }
+}
