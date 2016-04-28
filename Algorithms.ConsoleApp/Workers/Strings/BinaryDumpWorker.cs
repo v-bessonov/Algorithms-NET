d13e6f2282d306a28cfdc4fed6b89e1b9bcb68f7
vladdnc <vladdnc@mail.ru>
2016-04-28 17:57:16 +0300

    Added BinaryDump

---
+++ Algorithms.ConsoleApp/Workers/Strings/BinaryDumpWorker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Algorithms.ConsoleApp.Attributes;
+using Algorithms.ConsoleApp.Interfaces;
+using Algorithms.Core.Helpers;
+using Algorithms.Core.InOut;
+using Algorithms.Core.Strings;
+
+namespace Algorithms.ConsoleApp.Workers.Strings
+{
+    [ConsoleCommand("BinaryDump", "Reads in a binary file and writes out the bits, N per line.")]
+    public class BinaryDumpWorker : IWorker
+    {
+        public void Run()
+        {
+
+            Console.WriteLine("Choose file:"); // Prompt
+            Console.WriteLine("1 - abra.txt"); // Prompt
+            Console.WriteLine("or quit"); // Prompt
+
+            var fileNumber = Console.ReadLine();
+            var fieName = string.Empty;
+            switch (fileNumber)
+            {
+                case "1":
+                    fieName = "abra.txt";
+                    break;
+                case "quit":
+                    return;
+                default:
+                    return;
+            }
+
+
+            var @in = new In($"Files\\Strings\\{fieName}");
+            var content = @in.ReadAll();
+            var binaryDump = new BinaryDump(content, 16);
+            binaryDump.Dump();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Algorithms.Core/Strings/BinaryDump.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Algorithms.Core.InOut;
+
+namespace Algorithms.Core.Strings
+{
+    /// <summary>
+    /// The <tt>BinaryDump</tt> class provides a client for displaying the contents
+    /// of a binary file in binary.
+    /// <p/>
+    /// For more full-featured versions, see the Unix utilities
+    /// <tt>od</tt> (octal dump) and <tt>hexdump</tt> (hexadecimal dump).
+    /// <p/>
+    /// </summary>
+    public class BinaryDump
+    {
+        private readonly string _text;
+        private readonly int _bitsPerLine;
+
+        public BinaryDump(string text, int bitsPerLine)
+        {
+            _text = text;
+            _bitsPerLine = bitsPerLine;
+        }
+
+        public void Dump()
+        {
+            var binaryOut = new BinaryOut(_bitsPerLine);
+            binaryOut.ResetTotal();
+            binaryOut.Write(_text);
+            Console.WriteLine($"{binaryOut.TotalBits} bits");
+        }
+    }
+}
